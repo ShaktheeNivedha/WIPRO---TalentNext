b49520448a359b070c0c703f89f1687ce271101b
ShaktheeNivedha <shaktheenivedha@gmail.com>
2025-08-16 10:45:34 +0530

    Add files via upload

---
+++ two_triangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class two_triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(j + " ");
+            }
+            for(int j = 1; j <=(2 * (n - i)); j++){
+                System.out.print("  ");
+            }
+            for(int k = i; k >= 1; k--){
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+    }
+}
